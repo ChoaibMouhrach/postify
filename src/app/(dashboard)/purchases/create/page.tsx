4394d152aa8391d6a3acfa9147f10a894ab0c184
Choaib Mouhrach <cmouhrach@gmail.com>
2024-04-22 11:15:53 +0100

    Update purchases/create

--- src/app/(dashboard)/purchases/create/page.tsx
+++ src/app/(dashboard)/purchases/create/page.tsx
@@ -5,29 +5,15 @@ import {
   CardTitle,
 } from "@/client/components/ui/card";
 import { Create } from "./create";
-import { db } from "@/server/db";
-import { rscAuth } from "@/server/lib/action";
-import { products, suppliers } from "@/server/db/schema";
-import { eq } from "drizzle-orm";
 
 const Page = async () => {
-  const user = await rscAuth();
-
-  const ps = await db.query.products.findMany({
-    where: eq(products.userId, user.id),
-  });
-
-  const sups = await db.query.suppliers.findMany({
-    where: eq(suppliers.userId, user.id),
-  });
-
   return (
     <Card>
       <CardHeader>
         <CardTitle>New Purchase</CardTitle>
         <CardDescription>You can add new purchases from here.</CardDescription>
       </CardHeader>
-      <Create products={ps} suppliers={sups} />
+      <Create />
     </Card>
   );
 };
